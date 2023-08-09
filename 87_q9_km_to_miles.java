be6ccbbe2961cde58479f841c3783492ae212b43
Abujar Shaikh <103555266+abujarshaikh@users.noreply.github.com>
2023-08-09 23:21:33 +0530

    Add files via upload

---
+++ 87_q9_km_to_miles.java
@@ -0,0 +1,5 @@
+/*Write a program that asks the user to enter a distance in kilometers and then prints out
+how far that distance is in miles. There are 0.621371 miles in one kilometer.
+Sample I/O:
+How many kilometers?: 5.5
+5.500 kilometers is equal to 3.418 miles*/
