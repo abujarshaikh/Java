d6c2e0750d2a60d205237ead13e8dfe4d13533d2
Abujar Shaikh <103555266+abujarshaikh@users.noreply.github.com>
2023-08-09 23:59:37 +0530

    Add files via upload

---
+++ 89_q11_print_num.java
@@ -0,0 +1,14 @@
+/*Write a Java Program to display all numbers between 1 and 10 using for loop.
+Output:
+1
+2
+.
+.
+10*/
+class print {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(" " + i);
+        }
+    }
+}
