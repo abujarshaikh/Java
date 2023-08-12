01f4465965564f3a4aacaf572f7a5a98b8c2ee8c
Abujar Shaikh <103555266+abujarshaikh@users.noreply.github.com>
2023-08-12 20:58:08 +0530

    Add files via upload

---
+++ 114_q36.java
@@ -0,0 +1,13 @@
+// Write a program to find the number of and sum of all integers greater than 100 and less than 200 that are divisible by 7
+class X36 {
+    public static void main(String args[]) {
+        int sum = 0;
+        for (int i = 100; i < 200; i++) {
+            if (i % 7 == 0) {
+                sum += i;
+            }
+
+        }
+        System.out.println("sum: " + sum);
+    }
+}
