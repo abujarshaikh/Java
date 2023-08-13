39a9971b80499ef3bc43c410e3354d6b41ff5424
Abujar Shaikh <103555266+abujarshaikh@users.noreply.github.com>
2023-08-13 13:24:44 +0530

    Add files via upload

---
+++ 116_q38.java
@@ -0,0 +1,12 @@
+
+// Java Program: read the age of 20 persons and count the number of persons in the age group 50 -60
+import java.util.*; // import Scanner class from library
+
+class AgeGroup {
+    public static void main(String arg[]) {
+        int n = 20;
+        for (int i = n; i <= n; i++) {
+            System.out.println("Enter the age of people " + i);
+        }
+    }
+}
